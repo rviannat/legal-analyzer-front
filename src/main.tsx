56bbbaded518ee3a131a7d47d90bf072a09141ed
Rafael Vianna <119507755+rviannat@users.noreply.github.com>
2026-08-13 13:42:02 -0300

    feat(datajud): render CNJ audit overlay

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import DataJudOverlay from './DataJudOverlay'
 import './styles.css'
 import './layout-redesign.css'
+import './datajud.css'
 
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode><><App/><DataJudOverlay/></></React.StrictMode>
 )
