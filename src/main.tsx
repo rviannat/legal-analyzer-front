90f2f03879356df74fb6336588775930e73a4bed
Rafael Vianna <119507755+rviannat@users.noreply.github.com>
2026-08-13 16:58:11 -0300

    feat: place unified process search in main workspace

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import DataJudOverlay from './DataJudOverlay'
+import ProcessSearchBar from './ProcessSearchBar'
 import './styles.css'
 import './layout-redesign.css'
 import './datajud.css'
+import './process-search.css'
 
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><><App/><DataJudOverlay/></></React.StrictMode>
+  <React.StrictMode><><App/><ProcessSearchBar/><DataJudOverlay/></></React.StrictMode>
 )
