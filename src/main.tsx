24fd06e519568cd86987dc8f8d5cc42fc548b486
Rafael Vianna <119507755+rviannat@users.noreply.github.com>
2026-08-14 02:53:30 -0300

    feat: conectar central de processos ao menu Processos

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import ProcessosCentral from './ProcessosCentral'
 import './styles.css'
 import './layout-redesign.css'
+import './processos-central.css'
 
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode><><App /><ProcessosCentral /></></React.StrictMode>
 )
